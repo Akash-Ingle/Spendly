32c2cd0bdfe0186c0f571ab4d5a64d649de7ca88
Akash-Ingle <akashingle563@gmail.com>
2025-07-07 19:41:55 +0530

    Wrote add, get and delete incomes

---
+++ backend/controllers/incomeController.js
@@ -0,0 +1,54 @@
+const User = require('../models/User');
+const Income = require('../models/Income');
+
+// Add Income Source
+exports.addIncome = async (req, res) => {
+    const userId = req.user.id;
+    try {
+        const {icon, source, amount, date} = req.body;
+
+        // Validation: Check for missing fields
+        if(!source || !amount || !date) {
+            return res.status(400).json({message: 'All fields are required.'});
+        }
+
+        const newIncome = new Income({
+            userId,
+            icon,
+            source,
+            amount,
+            date: new Date(date)
+        });
+        await newIncome.save();
+        res.status(200).json(newIncome);
+    }
+    catch (error) {
+        res.status(500).json({message: 'Server error'});
+    }
+}
+
+// Get All Income Source
+exports.getAllIncome = async (req, res) => {
+    const userId = req.user.id;
+    try {
+        const income = await Income.find({userId}).sort({date: -1});
+        res.json(income);
+    } catch (error) {
+        res.status(500).json({message: 'Server error'});
+    }
+}
+
+// Delete Income Source
+exports.deleteIncome = async (req, res) => {
+    const userId = req.user.id;
+    try {
+        await Income.findByIdAndDelete(req.params.id);
+        res.json({message: 'Income deleted successfully'});
+    }
+    catch (error) {
+        res.status(500).json({message: 'Server error'});
+    }
+}
+
+// Download Excel
+exports.downloadIncomeExcel = async (req, res) => {}

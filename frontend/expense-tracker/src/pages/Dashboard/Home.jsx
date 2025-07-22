0347e9d40d69e39f011f04c3636c0aa61556b4bb
Akash-Ingle <akashingle563@gmail.com>
2025-07-22 20:45:12 +0530

    Worked on /dashboard

--- frontend/expense-tracker/src/pages/Dashboard/Home.jsx
+++ frontend/expense-tracker/src/pages/Dashboard/Home.jsx
@@ -9,6 +9,7 @@ import InfoCard from '../../components/Cards/InfoCard';
 import { LuHandCoins, LuWalletMinimal } from 'react-icons/lu';
 import {IoMdCard} from "react-icons/io"
 import { addThousandSeparator } from '../../utils/helper';
+import RecentTransactions from '../../components/Dashboard/RecentTransactions';
 
 const Home = () => {
   useUserAuth();
@@ -51,6 +52,26 @@ const Home = () => {
             label="Total Balance"
             value={addThousandSeparator(dashboardData?.totalBalance || 0)}
             color="bg-primary"
+          />
+
+          <InfoCard
+            icon={<LuWalletMinimal/>}
+            label="Total Income"
+            value={addThousandSeparator(dashboardData?.totalIncome || 0)}
+            color="bg-orange-500"
+          />
+
+          <InfoCard
+            icon={<LuHandCoins/>}
+            label="Total Expense"
+            value={addThousandSeparator(dashboardData?.totalExpenses || 0)}
+            color="bg-red-500"
+          />
+        </div>
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-6 mt-6">
+          <RecentTransactions
+            transactions = {dashboardData?.recentTransactions}
+            onSeeMore={() => navigate("/expense")}
           />
         </div>
       </div>

53f3c5cae9e307e8e014c2003a1542c03d02fb6b
Akash-Ingle <akashingle563@gmail.com>
2025-08-01 19:10:13 +0530

    Worked on Income Dashboard

--- frontend/expense-tracker/src/pages/Dashboard/Income.jsx
+++ frontend/expense-tracker/src/pages/Dashboard/Income.jsx
@@ -1,8 +1,55 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import DashboardLayout from '../../components/layouts/DashboardLayout'
+import IncomeOverview from '../../components/Dashboard/IncomeOverview';
+import axiosInstance from '../../utils/axiosInstance';
+import { API_PATHS } from '../../utils/apiPaths';
+import Modal from '../../components/Modal';
 
 const Income = () => {
+  const [incomeData, setIncomeData] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [openDeleteAlert, setOpenDeleteAlert] = useState({
+    show:false,
+    data:null
+  });
   const [openAddIncomeModal, setOpenAddIncomeModal] = useState(false);
+
+  // Get all income details
+  const fetchIncomeDetails = async () => {
+    if(loading) return;
+    setLoading(true);
+    try {
+      const response = await axiosInstance.get(
+        `${API_PATHS.INCOME.GET_ALL_INCOME}`
+      )
+      if(response.data) {
+        setIncomeData(response.data);
+      }
+    }
+    catch (error) {
+      console.log("Something wen wrong. Please try again.", error);
+    }
+    finally {
+      setLoading(false);
+    }
+  };
+
+  // Handle add income
+  const handleAddIncome = async (income) => {};
+
+  // Delete income
+  const deleteIncome = async (id) => {};
+
+  // Handle download income details
+  const handleDownloadIncomeDetails = async () => {};
+
+  useEffect(() => {
+    fetchIncomeDetails();
+
+    return () => {}
+  }, [])
+
+
   return (
     <DashboardLayout activeMenu="Income">
       <div className='my-5 mx-auto'>
@@ -14,6 +61,13 @@ const Income = () => {
             />
           </div>
         </div>
+        <Modal
+          isOpen={openAddIncomeModal}
+          onClose={() => setOpenAddIncomeModal(false)}
+          title="Add Income"
+          >
+            <div></div>
+        </Modal>
       </div>
     </DashboardLayout>
   )
